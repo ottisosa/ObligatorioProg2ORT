f6635f4a22ba295e445e4bce48581501567e44a4
octavio sosa <121409253+ottisosa@users.noreply.github.com>
2025-10-13 15:15:49 -0300

    Estructura del proyecto

---
+++ src/obligatorio/Obligatorio.java
@@ -0,0 +1,15 @@
+package obligatorio;
+
+// Autores: Santiago Quintana (327886), Octavio Sosa (363131)
+
+public class Obligatorio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        
+    }
+    
+}
---
+++ src/obligatorio/Partida.java
@@ -0,0 +1,7 @@
+package obligatorio;
+
+// Autores: Santiago Quintana (327886), Octavio Sosa (363131)
+
+public class Partida {
+    
+}
