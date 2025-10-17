e84d6b802e6bc9d4904b108e5097e7970b3784e6
ottisosa <ottisosa10@gmail.com>
2025-10-17 08:42:11 -0300

    avance
    av

--- obligatorio/src/obligatorio/Sistema.java
+++ obligatorio/src/obligatorio/Sistema.java
@@ -1,7 +1,36 @@
 package obligatorio;
 
 // Autores: Santiago Quintana (327886), Octavio Sosa (363131)
+import java.util.*;
 
 public class Sistema {
-    
+
+    private ArrayList<Jugador> listaJugadores = new ArrayList<>();
+
+    public ArrayList<Jugador> getListaJugadores() {
+        return listaJugadores;
+    }
+
+    public void addListaJugadores(Jugador Jug) {
+
+        listaJugadores.add(Jug);
+
+    }
+
+    public void mostrarJugadoresInvictos() {
+
+        for (Jugador jug : listaJugadores) {
+
+            if (jug.isInvicto() == true) {
+
+                System.out.println(jug);
+            }
+
+        }
+
+    }
+
+    public void mostrarRanking() {
+            
+    }
 }
